813a469d7f5da91d458b62737aaee5b02c678b24
hasanalgul <75197750+hasanalgul@users.noreply.github.com>
2020-12-13 13:53:42 +0300

    Update soru4.py

--- soru4.py
+++ soru4.py
@@ -1,7 +1,11 @@
+sayac=0
 for i in range(100,999):
     q=str(i)[0]
     w=str(i)[1]
     e=str(i)[2]
 
     if q!=w and q!=e and w!=e:
+        sayac = sayac+1
         print(i)
+
+print("Tekrar etmeyen sayı toplamı = "+str(sayac))
